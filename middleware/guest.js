1fd5655b51d730301a589c980d8cf989d4e4c81a
chanchai jeimvijack <kewell5@live.com>
2020-11-16 01:08:35 +0700

    test deploy

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
     if (store.state.auth.loggedIn) {
-    // check if login tests
+    // check if login testss
         return redirect("/");
     }
    
